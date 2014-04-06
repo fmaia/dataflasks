2edcf506f3fe6ef0bed9b33897c224ee86eb2526
fmaia <fmaia@di.uminho.pt>
2014-04-07 00:13:01 +0100

    added 10 thread config to yscb load.

--- src/simulation/MainSimulation.java
+++ src/simulation/MainSimulation.java
@@ -484,7 +484,7 @@ public class MainSimulation {
 			if (loglevel.equals("debug"))System.out.println("Scheduling YCSB run at "+runtime+" s");
 			ycsb.at(runtime,TimeUnit.SECONDS).queue().main("com.yahoo.ycsb.Client","-t","-s","-threads","1","-db","ycsbglue.StratusClient","-p","exportfile=ycsbRUN.txt",
 					"-p","stratus.ip="+ycsbip,"-p",
-					"stratus.port=65000","-p", "stratus.id=ycsbRun","-P", "workloads/workloadb");
+					"stratus.port=11000","-p", "stratus.id=ycsbRun","-P", "workloads/workloadb");
 			
 		} catch (IllegalArgumentException e) {
 			// TODO Auto-generated catch block
@@ -526,9 +526,9 @@ public class MainSimulation {
 			Runtime.getRuntime().exec("mkdir "+ycsbip);
 			Runtime.getRuntime().exec("cp -r workloads/ "+ycsbip);
 			if (loglevel.equals("debug"))System.out.println("Scheduling YCSB load at "+initload+" s");
-			ycsb.at(initload,TimeUnit.SECONDS).queue().main("com.yahoo.ycsb.Client","-load","-s","-threads","1","-db","ycsbglue.StratusClient","-p","exportfile=ycsbLOAD.txt",
+			ycsb.at(initload,TimeUnit.SECONDS).queue().main("com.yahoo.ycsb.Client","-load","-s","-threads","10","-db","ycsbglue.StratusClient","-p","exportfile=ycsbLOAD.txt",
 					"-p","stratus.ip="+ycsbip,"-p",
-					"stratus.port=64000","-p", "stratus.id=ycsbload","-P", "workloads/workloadb");
+					"stratus.port=10000","-p", "stratus.id=ycsbload","-P", "workloads/workloadb");
 			
 			
 		} catch (IllegalArgumentException e) {
