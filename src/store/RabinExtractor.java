515a8d71ce26d313998b69879fb8abe1499b1087
Francisco Maia <fmaia@di.uminho.pt>
2016-02-11 15:16:41 +0000

    bug fix in ExtractBlocks. We were failing to consider the empty string case

--- src/store/RabinExtractor.java
+++ src/store/RabinExtractor.java
@@ -19,16 +19,18 @@ public class RabinExtractor {
         String[] res = this.extractblocks(size,data);
         int pos=0;
         for( String el : res ){
-        	//BE VERY CAREFULL the number 7 is hardcoded due to the rabingenerator.c
-        	int ressize = new Integer(el.substring(el.length()-7));
-            byte[] resdata = new byte[ressize];
-            //System.arraycopy(data, pos, resdata, 0, size);
-            for (int i=0;i<ressize;i++){
-            	resdata[i]=data[pos+i];
-            }
-            blocks.add(new RabinBlock(el,ressize,resdata));
-            pos = pos+ressize;
-        	//System.out.println(el);
+        	if(!el.equals("")){
+        		//BE VERY CAREFULL the number 7 is hardcoded due to the rabingenerator.c
+            	int ressize = new Integer(el.substring(el.length()-7));
+                byte[] resdata = new byte[ressize];
+                //System.arraycopy(data, pos, resdata, 0, size);
+                for (int i=0;i<ressize;i++){
+                	resdata[i]=data[pos+i];
+                }
+                blocks.add(new RabinBlock(el,ressize,resdata));
+                pos = pos+ressize;
+            	//System.out.println(el);
+        	}	
         }
         //System.out.println("get blocks processed");
         return blocks;
